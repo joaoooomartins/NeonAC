e2f58658cdda13cd7207ac9608d12ed1b7789dbc
João Martins <shadowonerb11@gmail.com>
2026-08-23 05:25:21 -0300

    Deleted
    Deleted

--- earac-api/src/main/java/com/earac/api/check/CheckRegistry.java
+++
@@ -1,9 +0,0 @@
-package com.earac.api.check;
-
-/**
- * Used by the core to accept externally registered checks (3rd-party plugins or modules).
- */
-public interface CheckRegistry {
-
-    void register(Check check);
-}
--- earac-api/src/main/java/com/earac/api/events/EarACEvent.java
+++
@@ -1,7 +0,0 @@
-package com.earac.api.events;
-
-/**
- * Marker for all EarAC events. Core ships Bukkit-backed implementations of these.
- */
-public interface EarACEvent {
-}
